7d26cfc49d6d5c7980aba3a2e457a2e55473ef26
yankunSun <becomebig@163.com>
2020-12-27 11:29:40 +0800

    提交本地

---
+++ src/main/java/com/sunyk/base/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.sunyk.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseApplication {
+//  https://github.com/caizhaokai/spring-boot-demo redis整合待测试  https://github.com/yankunSun/base.git
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+
+}
