188e49c3d5c6ad2b57c420cf9046db427787750d
yankunSun <2411465321@qq.com>
2020-12-30 23:51:18 +0800

    12

--- src/main/java/com/sunyk/base/BaseApplication.java
+++ src/main/java/com/sunyk/base/BaseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BaseApplication {
-//  https://github.com/caizhaokai/spring-boot-demo redis整合待测试  https://github.com/yankunSun/base.git
+//  https://github.com/caizhaokai/spring-boot-demo redis整合待测试
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class, args);
     }
