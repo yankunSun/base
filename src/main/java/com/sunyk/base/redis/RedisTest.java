70b6aa19bfa3025cca492de1905c55dd31b3b989
yankunSun <becomebig@163.com>
2021-01-01 11:36:02 +0800

    添加模块测试类

---
+++ src/main/java/com/sunyk/base/redis/RedisTest.java
@@ -0,0 +1,7 @@
+package com.sunyk.base.redis;
+
+public class RedisTest {
+    public static void main(String[] args) {
+        System.out.println("redisTest:main:");
+    }
+}
---
+++ src/main/java/com/sunyk/base/security/SecurityTest.java
@@ -0,0 +1,7 @@
+package com.sunyk.base.security;
+
+public class SecurityTest {
+    public static void main(String[] args) {
+        System.out.println("SecurityTest:main:");
+    }
+}
